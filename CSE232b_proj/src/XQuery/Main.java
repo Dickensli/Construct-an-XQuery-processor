62c6aecdc5b7a517ffecab88efc4fbc3145b2434
Dickensli <32367491+Dickensli@users.noreply.github.com>
2018-02-22 17:51:38 -0800

    Update main function to extract input from input.txt.

--- CSE232b_proj/src/XQuery/Main.java
+++ CSE232b_proj/src/XQuery/Main.java
@@ -1,20 +1,28 @@
 package XQuery;
 import org.antlr.v4.runtime.*;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.io.*;
 
 public class Main {
     public static void main (String[] args) throws IOException{
-        String XQuery = "\"1235\"";
-        ANTLRInputStream in = new ANTLRInputStream(XQuery);
-        XQueryLexer lexer = new XQueryLexer(in);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        XQueryParser parser = new XQueryParser(tokens);
-        MyXQueryVisitor visitor = new MyXQueryVisitor();
-        ArrayList res = visitor.visit(parser.xq());
-        Output out = new Output("output.txt", XQuery);
-        out.printXML(res);
+        try
+        {
+            String XQuery = new String ( Files.readAllBytes( Paths.get("input.txt") ) );
+            ANTLRInputStream in = new ANTLRInputStream(XQuery);
+            XQueryLexer lexer = new XQueryLexer(in);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            XQueryParser parser = new XQueryParser(tokens);
+            MyXQueryVisitor visitor = new MyXQueryVisitor();
+            ArrayList res = visitor.visit(parser.xq());
+            Output out = new Output("output.txt", XQuery);
+            out.printXML(res);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
-
